d7b139fe3adbae5424fd1b97b70b736ebcdb9702
omar azaz <cool_omer@hotmail.co.uk>
2022-03-08 16:24:24 +0500

    handle empty report

--- src/controllers/report.js
+++ src/controllers/report.js
@@ -47,16 +47,23 @@ const getWeeklyReport = async (req, res) => {
       "date timeIn timeOut userName"
     ).populate("organizationID", "users");
 
-    const diff = moment(today).diff(startOfWeek, "days") + 1;
-    const percentageAttendance = Math.floor(
-      (attendances.length * 100) /
-        (attendances[0].organizationID.users.length * diff)
-    );
+    if (attendances.length) {
+      const diff = moment(today).diff(startOfWeek, "days") + 1;
+      const percentageAttendance = Math.floor(
+        (attendances.length * 100) /
+          (attendances[0].organizationID.users.length * diff)
+      );
 
-    res.json({
-      data: attendances,
-      percentageAttendance,
-    });
+      res.json({
+        data: attendances,
+        percentageAttendance,
+      });
+    } else {
+      res.json({
+        data: attendances,
+        percentageAttendance: 0,
+      });
+    }
   } catch (err) {
     res.json({
       error: "Error: Couldn't generate weekly report.",
@@ -80,23 +87,30 @@ const getMonthlyReport = async (req, res) => {
       "date timeIn timeOut userName"
     ).populate("organizationID", "users");
 
-    const diff = moment(today).diff(startOfMonth, "days") + 1;
+    if (attendances.length) {
+      const diff = moment(today).diff(startOfMonth, "days") + 1;
 
-    today = moment().format("YYYY-MM-ddd");
-    startOfMonth = moment(startOfMonth, "YYYY-MM-DD").format("YYYY-MM-ddd");
+      today = moment().format("YYYY-MM-ddd");
+      startOfMonth = moment(startOfMonth, "YYYY-MM-DD").format("YYYY-MM-ddd");
 
-    var workDays = diff - Math.floor(diff / 7);
-    if (today.includes("Sun") || startOfMonth.includes("Sun")) workDays -= 1;
+      var workDays = diff - Math.floor(diff / 7);
+      if (today.includes("Sun") || startOfMonth.includes("Sun")) workDays -= 1;
 
-    const percentageAttendance = Math.floor(
-      (attendances.length * 100) /
-        (workDays * attendances[0].organizationID.users.length)
-    );
+      const percentageAttendance = Math.floor(
+        (attendances.length * 100) /
+          (workDays * attendances[0].organizationID.users.length)
+      );
 
-    res.json({
-      data: attendances,
-      percentageAttendance,
-    });
+      res.json({
+        data: attendances,
+        percentageAttendance,
+      });
+    } else {
+      res.json({
+        data: attendances,
+        percentageAttendance: 0,
+      });
+    }
   } catch (err) {
     res.json({
       error: "Error: Couldn't generate monthly report.",
@@ -125,23 +139,30 @@ const getThreeMonthsReport = async (req, res) => {
       "date timeIn timeOut userName"
     ).populate("organizationID", "users");
 
-    const diff = moment(today).diff(last3Months, "days") + 1;
+    if (attendances.length) {
+      const diff = moment(today).diff(last3Months, "days") + 1;
 
-    today = moment().format("YYYY-MM-ddd");
-    last3Months = moment(last3Months, "YYYY-MM-DD").format("YYYY-MM-ddd");
+      today = moment().format("YYYY-MM-ddd");
+      last3Months = moment(last3Months, "YYYY-MM-DD").format("YYYY-MM-ddd");
 
-    var workDays = diff - Math.floor(diff / 7);
-    if (today.includes("Sun") || last3Months.includes("Sun")) workDays -= 1;
+      var workDays = diff - Math.floor(diff / 7);
+      if (today.includes("Sun") || last3Months.includes("Sun")) workDays -= 1;
 
-    const percentageAttendance = Math.floor(
-      (attendances.length * 100) /
-        (workDays * attendances[0].organizationID.users.length)
-    );
+      const percentageAttendance = Math.floor(
+        (attendances.length * 100) /
+          (workDays * attendances[0].organizationID.users.length)
+      );
 
-    res.json({
-      data: attendances,
-      percentageAttendance,
-    });
+      res.json({
+        data: attendances,
+        percentageAttendance,
+      });
+    } else {
+      res.json({
+        data: attendances,
+        percentageAttendance: 0,
+      });
+    }
   } catch (err) {
     res.json({
       error: "Error: Couldn't generate 3 months report.",
@@ -170,24 +191,31 @@ const getSixMonthsReport = async (req, res) => {
       "date timeIn timeOut userName"
     ).populate("organizationID", "users");
 
-    const diff = moment(today).diff(last6Months, "days") + 1;
+    if (attendances.length) {
+      const diff = moment(today).diff(last6Months, "days") + 1;
 
-    today = moment().format("YYYY-MM-ddd");
-    last6Months = moment(last6Months, "YYYY-MM-DD").format("YYYY-MM-ddd");
+      today = moment().format("YYYY-MM-ddd");
+      last6Months = moment(last6Months, "YYYY-MM-DD").format("YYYY-MM-ddd");
 
-    var workDays = diff - Math.floor(diff / 7);
+      var workDays = diff - Math.floor(diff / 7);
 
-    if (today.includes("Sun") || last6Months.includes("Sun")) workDays -= 1;
+      if (today.includes("Sun") || last6Months.includes("Sun")) workDays -= 1;
 
-    const percentageAttendance = Math.floor(
-      (attendances.length * 100) /
-        (workDays * attendances[0].organizationID.users.length)
-    );
+      const percentageAttendance = Math.floor(
+        (attendances.length * 100) /
+          (workDays * attendances[0].organizationID.users.length)
+      );
 
-    res.json({
-      data: attendances,
-      percentageAttendance,
-    });
+      res.json({
+        data: attendances,
+        percentageAttendance,
+      });
+    } else {
+      res.json({
+        data: attendances,
+        percentageAttendance: 0,
+      });
+    }
   } catch (err) {
     res.json({
       error: "Error: Couldn't generate 6 months report.",
@@ -212,22 +240,29 @@ const getUserReport = async (req, res) => {
       "date timeIn timeOut"
     ).sort({ _id: -1 });
 
-    const diff = moment(today).diff(startOfMonth, "days") + 1;
+    if (attendances.length) {
+      const diff = moment(today).diff(startOfMonth, "days") + 1;
 
-    today = moment().format("YYYY-MM-ddd");
-    startOfMonth = moment(startOfMonth, "YYYY-MM-DD").format("YYYY-MM-ddd");
+      today = moment().format("YYYY-MM-ddd");
+      startOfMonth = moment(startOfMonth, "YYYY-MM-DD").format("YYYY-MM-ddd");
 
-    var workDays = diff - Math.floor(diff / 7);
-    if (today.includes("Sun") || startOfMonth.includes("Sun")) workDays -= 1;
+      var workDays = diff - Math.floor(diff / 7);
+      if (today.includes("Sun") || startOfMonth.includes("Sun")) workDays -= 1;
 
-    const percentageAttendance = Math.floor(
-      (attendances.length * 100) / workDays
-    );
+      const percentageAttendance = Math.floor(
+        (attendances.length * 100) / workDays
+      );
 
-    res.json({
-      data: attendances,
-      percentageAttendance,
-    });
+      res.json({
+        data: attendances,
+        percentageAttendance,
+      });
+    } else {
+      res.json({
+        data: attendances,
+        percentageAttendance: 0,
+      });
+    }
   } catch (err) {
     res.json({
       error: `Error: Couldn't generate report for user.`,
@@ -258,7 +293,7 @@ const getFilteredUserReport = async (req, res) => {
 
     var percentageAttendance = 0;
 
-    if (attendances.length > 0) {
+    if (attendances.length) {
       var diff = from == to ? 0 : moment(to).diff(from, "days") + 1;
       var workDays = diff - Math.floor(diff / 7);
 
@@ -311,7 +346,7 @@ const getCustomReport = async (req, res) => {
 
     var percentageAttendance = 0;
 
-    if (attendances.length > 0) {
+    if (attendances.length) {
       var diff = from == to ? 0 : moment(to).diff(from, "days") + 1;
       var workDays = diff - Math.floor(diff / 7);
 
@@ -320,14 +355,12 @@ const getCustomReport = async (req, res) => {
 
       if (workDays > 0) {
         if (to.includes("Sun") || from.includes("Sun")) workDays -= 1;
-      }
 
-      if (workDays > 0)
         percentageAttendance = Math.floor(
           (attendances.length * 100) /
             (workDays * attendances[0].organizationID.users.length)
         );
-      else percentageAttendance = 100;
+      } else percentageAttendance = 0;
     }
 
     res.json({
